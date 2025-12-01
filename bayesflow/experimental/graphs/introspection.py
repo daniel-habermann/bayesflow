671bd10893e9d2a4b1ff320a8211c98a9f3f9fed
Danie Habermann <133031176+daniel-habermann@users.noreply.github.com>
2025-12-01 05:32:47 +0100

    add original_node_names function to introspection module

--- bayesflow/experimental/graphs/introspection.py
+++ bayesflow/experimental/graphs/introspection.py
@@ -8,8 +8,18 @@
 # method to identify which inference network needs which conditions
 # method to identify if the conditions are group-wise or combined
 
-def node_names_in_simulation_graph(inverted_graph):
+# maps node names of inverted graph to node names in corresponding SimulationGraph
+def original_node_names(inverted_graph):
     mapping = {}
     
     for node in inverted_graph.nodes:
-        expanded_node = self.expanded_graph.nodes[node]
+        expanded_node = inverted_graph.expanded_graph.nodes[node]
+        
+        if expanded_node["merged_from"] != []:
+            mapping[node] = expanded_node["merged_from"][0]
+        elif expanded_node["previous_names"] == []:
+            mapping[node] = node
+        else:
+            mapping[node] = expanded_node["previous_names"][0]
+
+    return mapping
